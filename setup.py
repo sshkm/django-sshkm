8229dbb99afd70a90e17221d17fbc2307e7c055c
sshkm <sshkm@guruz.at>
2017-02-14 20:35:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         #'enum34;python_version<"3.4"',
     ],
     dependency_links=[
-        "https://github.com/sshkm/django-menu/tarball/0.1.10.1#egg=menu-0.1.10.1"
+        "https://github.com/sshkm/django-menu/tarball/0.1.10.1#egg=django-menu-0.1.10.1"
     ],
     data_files=[
         ('/etc/sshkm', ['sshkm.conf']),
