59cb0c4ee140e9958b35f14d129f1caf2b7558dd
Peter Loeffler <peter.loeffler@guruz.at>
2017-02-15 18:49:17 +0100

    cleanups

--- sshkm/tasks.py
+++ sshkm/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from sshkm.views.deploy import DeployKeys
+from sshkm.views.deploy import DeployKeys, GetHostKeys
 
 # task to deploy ssh-keys to hosts
 @shared_task
