846ae17115fd33c812bf8afb10c5d2982e39e593
Peter Loeffler <peter@latitude.guruz.home>
2017-02-17 20:22:40 +0100

    GUI cleanups

--- sshkm/static/custom.js
+++ sshkm/static/custom.js
@@ -1,4 +1,38 @@
 // select all functionality
 $(".checkAll").click(function () {
-    $(this).closest('form').find(':checkbox').prop('checked', $(this).prop('checked'));
+  $(this).closest('form').find(':checkbox').prop('checked', $(this).prop('checked'));
 });
+
+function checkboxCheck(action, element, title_confirm, message_confirm, title_noselect, message_noselect) {
+  var checkboxs=document.getElementsByName(element);
+  var okay=false;
+
+  for(var i=0,l=checkboxs.length;i<l;i++) {
+    if(checkboxs[i].checked) {
+      okay=true;
+      break;
+    }
+  }
+
+  if(okay) {
+    BootstrapDialog.confirm(
+      {
+        title: title_confirm,
+        message: message_confirm,
+        callback: function(result) {
+          if(result) {
+            action();
+          }
+        }
+      }
+    );
+  } else {
+    BootstrapDialog.alert(
+      {
+        title: title_noselect,
+        message: message_noselect
+      }
+    );
+  }
+}
+
