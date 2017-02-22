8a04f3b21cec20bc3ceb9613312b2c2ce52c1a8e
Peter Loeffler <peter.loeffler@guruz.at>
2017-02-22 11:17:29 +0100

    postscript

--- setup.py
+++ setup.py
@@ -15,22 +15,23 @@ else:
 
 # post installation tasks
 class post_install():
-    from django.utils.crypto import get_random_string
+    def run(self):
+        from django.utils.crypto import get_random_string
 
-    print('running get_production_ready...')
+        print('running get_production_ready...')
 
-    SECRET_KEY = get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
+        SECRET_KEY = get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
 
-    f = open('sshkm/settings.py', 'r')
-    filedata = f.read()
-    f.close()
+        f = open('sshkm/settings.py', 'r')
+        filedata = f.read()
+        f.close()
 
-    newdata = filedata.replace("'SECRET_KEY_PLACEHOLDER'", "'"+SECRET_KEY+"'")
-    newdata = newdata.replace("DEBUG = True", "DEBUG = False")
+        newdata = filedata.replace("'SECRET_KEY_PLACEHOLDER'", "'"+SECRET_KEY+"'")
+        newdata = newdata.replace("DEBUG = True", "DEBUG = False")
 
-    f = open('sshkm/settings.py', 'w')
-    f.write(newdata)
-    f.close()
+        f = open('sshkm/settings.py', 'w')
+        f.write(newdata)
+        f.close()
 
 version = '0.1.2'
 
