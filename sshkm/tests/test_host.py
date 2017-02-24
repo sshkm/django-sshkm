f9bfcbe841cf9bbc3105aa5348cc76118f3a0706
Peter Loeffler <peter.loeffler@guruz.at>
2017-02-24 19:57:40 +0100

    test travis

--- sshkm/tests/test_host.py
+++ sshkm/tests/test_host.py
@@ -11,7 +11,6 @@ class TestHost(TestCase):
         self.client.post('/login/', {'username': self.username, 'password': self.password})
 
     def test_host_deploy_single(self):
-        pass
-        #response = self.client.get('/host/deploy/', {'id': 1}, follow=True)
-        #self.assertContains(response, 'deployed', status_code=200)
-        #self.assertEqual(Host.objects.get(id=1).status, "SUCCESS")
+        response = self.client.get('/host/deploy/', {'id': 1}, follow=True)
+        self.assertContains(response, 'deployed', status_code=200)
+        self.assertEqual(Host.objects.get(id=1).status, "SUCCESS")
