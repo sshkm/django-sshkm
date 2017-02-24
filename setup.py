628d236006e7ed23569da864b4299d373facb289
Peter Loeffler <peter.loeffler@guruz.at>
2017-02-24 08:54:41 +0100

    finalize setup.py, enhance README

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os, sys, site
+
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
@@ -56,18 +57,35 @@ setup(
     url='https://github.com/sshkm/django-sshkm',
     download_url='https://github.com/sshkm/django-sshkm/archive/' + version + '.zip',
     author='Peter Loeffler',
-    author_email='peter.loeffler@guruz.at',
+    author_email='sshkm@guruz.at',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Intended Audience :: System Administrators',
         'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Natural Language :: German',
+        'Operating System :: Unix',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Utilities',
+        'Topic :: System :: Systems Administration',
     ],
     install_requires=[
         'setuptools',
