dd5097ba0cfdf63cd8b754e1cf2b02318b7b78f3
Peter Loeffler <peter.loeffler@guruz.at>
2017-02-24 12:19:33 +0100

    add fixtures for tests and write test case for single host deployment

--- sshkm/tests/test_host.py
+++ sshkm/tests/test_host.py
@@ -1,14 +1,16 @@
 from django.test import TestCase, Client
+from sshkm.models import Host
 
 class TestHost(TestCase):
+    fixtures = ['sshkm/tests/fixtures/testdata.json']
+
     def setUp(self):
-        pass
+        self.username = "admin"
+        self.password = "admin"
+
+        self.client.post('/login/', {'username': self.username, 'password': self.password})
 
     def test_host_deploy_single(self):
-        #c = Client()
-        #csrf_client = Client(enforce_csrf_checks=True)
-        #response = self.client.get("/path/to/resource/pk/")
-        response = self.client.post('/group/', {'username': 'admin', 'password': 'admin'})
-        response = self.client.get('/host/deploy/?id=1')
-        #self.assertContains(response, '<h1>Documentation</h1>', status_code=200)
-        #self.assertTemplateUsed(response, 'group/list.html')
+        response = self.client.get('/host/deploy/', {'id': 1}, follow=True)
+        self.assertContains(response, 'deployed', status_code=200)
+        self.assertEqual(Host.objects.get(id=1).status, "SUCCESS")
