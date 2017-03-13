2c339c2f7def8e9aac09548977805d4a260beeff
Peter Loeffler <peter.loeffler@guruz.at>
2017-03-13 14:07:55 +0100

    setup issues

--- sshkm/settings.py
+++ sshkm/settings.py
@@ -1,6 +1,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 SECRET_KEY = 'SECRET_KEY_PLACEHOLDER'
 
@@ -79,7 +80,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR + '/sshkm', 'db.sqlite3'),
     }
 }
 
